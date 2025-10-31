66baba85cc4c7f9f1dc84ed64647726e26acd176
Arthi Gokarn <arthig@microsoft.com>
2025-10-31 22:18:41 +0000

    Move changes from main to attestation library.

--- src/migtd/src/bin/migtd/main.rs
+++ src/migtd/src/bin/migtd/main.rs
@@ -10,7 +10,7 @@ extern crate alloc;
 use core::future::poll_fn;
 use core::task::Poll;
 
-use core::ffi::c_void;
+//use core::ffi::c_void;
 use log::{error, info};
 use migtd::event_log::TEST_DISABLE_RA_AND_ACCEPT_ALL_EVENT;
 use migtd::migration::data::MigrationInformation;
@@ -20,10 +20,6 @@ use migtd::{config, event_log, migration};
 use sha2::{Digest, Sha384};
 use spin::Mutex;
 use tdx_tdcall::tdreport;
-
-extern "C" {
-    pub fn servtd_get_quote(tdquote_req_buf: *mut core::ffi::c_void, len: u64) -> i32;
-}
 
 // Local trait to convert TdInfo to bytes without external dependency
 trait TdInfoAsBytes {
@@ -59,174 +55,13 @@ pub extern "C" fn main() {
         td_benchmark::StackProfiling::init(0x5a5a_5a5a_5a5a_5a5a, 0xd000);
     }
 
-    runtime_main()
+    runtime_main();
 }
 
 // AzCVMEmu entry point - standard Rust main function
 #[cfg(feature = "AzCVMEmu")]
 fn main() {
     cvmemu::main();
-}
-
-use alloc::vec;
-use alloc::vec::Vec;
-
-#[derive(Debug)]
-pub enum Error {
-    InvalidRootCa,
-    InitHeap,
-    GetQuote,
-    VerifyQuote,
-    InvalidOutput,
-    InvalidQuote,
-    OutOfMemory,
-}
-
-const TD_QUOTE_SIZE: usize = 0x2000;
-const SERVTD_REQ_BUF_SIZE: usize = 16 * 4 * 1024; // 16 pages
-
-struct ServtdTdxQuoteHdr {
-    /* Quote version, filled by TD */
-    version: u64,
-    /* Status code of Quote request, filled by VMM */
-    status: u64,
-    /* Length of TDREPORT, filled by TD */
-    in_len: u32,
-    /* Length of Quote, filled by VMM */
-    out_len: u32,
-    /* Actual Quote data or TDREPORT on input */
-    data: [u64; 0],
-}
-
-#[derive(Debug)]
-pub enum TdxAttestError {
-    TdxAttestSuccess = 0x0000,
-    ///< Success
-    TdxAttestErrorUnexpected = 0x0001,
-    ///< Unexpected error
-    TdxAttestErrorInvalidParameter = 0x0002,
-    ///< The parameter is incorrect
-    TdxAttestErrorOutOfMemory = 0x0003,
-    ///< Not enough memory is available to complete this operation
-    TdxAttestErrorVsockFailure = 0x0004,
-    ///< vsock related failure
-    TdxAttestErrorReportFailure = 0x0005,
-    ///< Failed to get the TD Report
-    TdxAttestErrorExtendFailure = 0x0006,
-    ///< Failed to extend rtmr
-    TdxAttestErrorNotSupported = 0x0007,
-    ///< Request feature is not supported
-    TdxAttestErrorQuoteFailure = 0x0008,
-    ///< Failed to get the TD Quote
-    TdxAttestErrorBusy = 0x0009,
-    ///< The device driver return busy
-    TdxAttestErrorDeviceFailure = 0x000a,
-    ///< Failed to acess tdx attest device
-    TdxAttestErrorInvalidRtmrIndex = 0x000b,
-    ///< Only supported RTMR index is 2 and 3
-    TdxAttestErrorUnsupportedAttKeyId = 0x000c,
-    ///< The platform Quoting infrastructure does not support any of the keys described in att_key_id_list
-    TdxAttestErrorMax,
-}
-
-impl TryFrom<i32> for TdxAttestError {
-    type Error = &'static str;
-
-    fn try_from(code: i32) -> Result<Self, Self::Error> {
-        match code {
-            0x0000 => Ok(TdxAttestError::TdxAttestSuccess),
-            0x0001 => Ok(TdxAttestError::TdxAttestErrorUnexpected),
-            0x0002 => Ok(TdxAttestError::TdxAttestErrorInvalidParameter),
-            0x0003 => Ok(TdxAttestError::TdxAttestErrorOutOfMemory),
-            0x0004 => Ok(TdxAttestError::TdxAttestErrorVsockFailure),
-            0x0005 => Ok(TdxAttestError::TdxAttestErrorReportFailure),
-            0x0006 => Ok(TdxAttestError::TdxAttestErrorExtendFailure),
-            0x0007 => Ok(TdxAttestError::TdxAttestErrorNotSupported),
-            0x0008 => Ok(TdxAttestError::TdxAttestErrorQuoteFailure),
-            0x0009 => Ok(TdxAttestError::TdxAttestErrorBusy),
-            0x000a => Ok(TdxAttestError::TdxAttestErrorDeviceFailure),
-            0x000b => Ok(TdxAttestError::TdxAttestErrorInvalidRtmrIndex),
-            0x000c => Ok(TdxAttestError::TdxAttestErrorUnsupportedAttKeyId),
-            _ => Err("Unknown TDX attestation error code"),
-        }
-    }
-}
-
-pub fn get_quote_internal(td_report: &[u8]) -> Result<Vec<u8>, TdxAttestError> {
-    let mut quote = vec![0u8; TD_QUOTE_SIZE];
-    let mut quote_size = TD_QUOTE_SIZE as u32;
-
-    let mut get_quote_blob = vec![0u8; SERVTD_REQ_BUF_SIZE];
-
-    // Dump header
-    let hdr = ServtdTdxQuoteHdr {
-        version: 1,
-        status: 0,
-        in_len: td_report.len() as u32,
-        out_len: quote_size as u32,
-        data: [],
-    };
-
-    let header_size = core::mem::size_of::<ServtdTdxQuoteHdr>();
-    let hdr_bytes =
-        unsafe { core::slice::from_raw_parts(&hdr as *const _ as *const u8, header_size) };
-    get_quote_blob[..header_size].copy_from_slice(hdr_bytes);
-
-    log::info!(
-        "Header size: {}, TD report size: {}\n",
-        header_size,
-        td_report.len()
-    );
-
-    log::info!("ServtdTdxQuoteHdr values before calling servtd_get_quote:\n");
-    log::info!("  version: {} ({:?})\n", hdr.version, hdr.version);
-    log::info!("  status: {} (0x{:x})\n", hdr.status, hdr.status);
-    log::info!("  in_len: {} ({:?})\n", hdr.in_len, hdr.in_len);
-    log::info!("  out_len: {} ({:?})\n", hdr.out_len, hdr.out_len);
-
-    // Copy TD report at data offset (after header)
-    get_quote_blob[header_size..header_size + td_report.len()].copy_from_slice(td_report);
-
-    // Dump the first 64 bytes of the blob for debugging
-    let dump_len = core::cmp::min(64, get_quote_blob.len());
-    log::info!(
-        "First {} bytes of get_quote_blob: {:02x?}\n",
-        dump_len,
-        &get_quote_blob[..dump_len]
-    );
-
-    let get_quote_blob_ptr = get_quote_blob.as_mut_ptr() as *mut c_void;
-    let servtd_get_quote_ret =
-        unsafe { servtd_get_quote(get_quote_blob_ptr, SERVTD_REQ_BUF_SIZE as u64) };
-
-    unsafe {
-        let hdr = get_quote_blob_ptr as *mut ServtdTdxQuoteHdr;
-        log::info!("ServtdTdxQuoteHdr values after calling servtd_get_quote:\n");
-        log::info!("  version: ({:?})\n", (*hdr).version);
-        log::info!("  status: (0x{:x})\n", (*hdr).status);
-        log::info!("  in_len: ({:?})\n", (*hdr).in_len);
-        log::info!("  out_len: ({:?})\n", (*hdr).out_len);
-        quote_size = (*hdr).out_len;
-        quote[..quote_size as usize]
-            .copy_from_slice(&get_quote_blob[header_size..header_size + quote_size as usize]);
-    };
-
-    if servtd_get_quote_ret != 0 {
-        log::error!(
-            "servtd_get_quote failed with error code: {}\n",
-            servtd_get_quote_ret
-        );
-        return Err(TdxAttestError::try_from(servtd_get_quote_ret).unwrap());
-    }
-
-    log::info!(
-        "get_quote_inner returned quote_size = {}, quote = {:?}\n",
-        quote_size,
-        &quote[..quote_size as usize]
-    );
-
-    quote.truncate(quote_size as usize);
-    Ok(quote)
 }
 
 pub fn runtime_main() {
@@ -254,7 +89,7 @@ pub fn runtime_main() {
 
     #[cfg(feature = "test_get_quote")]
     {
-        let td_quote = match get_quote_internal(td_report.as_bytes()) {
+        let td_quote = match attestation::get_quote_workaround(td_report.as_bytes()) {
             Ok(quote) => quote,
             Err(e) => {
                 error!("Failed to get quote - Error: {:?}\n", e);
@@ -262,50 +97,7 @@ pub fn runtime_main() {
                 error!(
                     "First 32 bytes of TD report: {:02x?}\n",
                     &td_report.as_bytes()[..32.min(td_report.as_bytes().len())]
-                );
-
-                // Log the specific error type
-                match e {
-                    TdxAttestError::TdxAttestErrorQuoteFailure => {
-                        error!("Error type: GetQuote - Failed to obtain quote from IGVMAgent\n")
-                    }
-                    TdxAttestError::TdxAttestErrorUnexpected => {
-                        error!("Error type: Unexpected - An unexpected error occurred\n")
-                    }
-                    TdxAttestError::TdxAttestErrorInvalidParameter => {
-                        error!("Error type: InvalidParameter - An invalid parameter was provided\n")
-                    }
-                    TdxAttestError::TdxAttestErrorOutOfMemory => {
-                        error!("Error type: OutOfMemory - Insufficient memory to complete the operation\n")
-                    }
-                    TdxAttestError::TdxAttestErrorVsockFailure => {
-                        error!("Error type: VsockFailure - A vsock related failure occurred\n")
-                    }
-                    TdxAttestError::TdxAttestErrorReportFailure => {
-                        error!("Error type: ReportFailure - Failed to get the TD Report\n")
-                    }
-                    TdxAttestError::TdxAttestErrorExtendFailure => {
-                        error!("Error type: ExtendFailure - Failed to extend RTMR\n");
-                    }
-                    TdxAttestError::TdxAttestErrorNotSupported => {
-                        error!(
-                            "Error type: NotSupported - The requested feature is not supported\n"
-                        )
-                    }
-                    TdxAttestError::TdxAttestErrorBusy => {
-                        error!("Error type: Busy - The device driver returned busy\n")
-                    }
-                    TdxAttestError::TdxAttestErrorDeviceFailure => {
-                        error!("Error type: DeviceFailure - Failed to access TDX attest device\n")
-                    }
-                    TdxAttestError::TdxAttestErrorInvalidRtmrIndex => {
-                        error!("Error type: InvalidRtmrIndex - Only RTMR index 2 and 3 are supported\n")
-                    }
-                    TdxAttestError::TdxAttestErrorUnsupportedAttKeyId => {
-                        error!("Error type: UnsupportedAttKeyId - The platform Quoting infrastructure does not support any of the keys described in att_key_id_list\n")
-                    }
-                    _ => error!("Error type: Other - {:?}\n", e),
-                }
+                );                
                 return;
             }
         };
